fa21ab556aa3021eb5c16c24122ff7f045315c1a
samuel0021 <atumosoft@outlook.com>
2025-11-13 21:23:41 -0300

    Comentários adicionados.

--- Commands/AsyncRelayCommand.cs
+++ Commands/AsyncRelayCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +10,21 @@ namespace sistecDesktop.Commands
 {
     public class AsyncRelayCommand : ICommand
     {
+        // Delegate para guardar o método a ser executado (deve retornar Task para ser assíncrono)
         private readonly Func<Task> _execute;
+        // Delegate opcional que define se o comando pode ou não ser executado no momento
         private readonly Func<bool> _canExecute;
+        // Flag interna que controla se o comando está executando no momento
         private bool _isExecuting;
 
+        // Construtor
         public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
+        // Permite que o WPF atualize automaticamente o estado dos botões e outras interfaces conectadas ao comando
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
@@ -37,18 +43,19 @@ namespace sistecDesktop.Commands
                 try
                 {
                     _isExecuting = true;
-                    RaiseCanExecuteChanged();
+                    RaiseCanExecuteChanged(); // Atualiza a UI (desativa botão)
 
                     await _execute();
                 }
                 finally
                 {
                     _isExecuting = false;
-                    RaiseCanExecuteChanged();
+                    RaiseCanExecuteChanged(); // Atualiza a UI (reativa botão)
                 }
             }
         }
 
+        // Revalidar se os comandos podem ser executados, disparando o evento de atualização na UI
         private void RaiseCanExecuteChanged()
         {
             CommandManager.InvalidateRequerySuggested();
